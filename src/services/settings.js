59cffd012be9540499f9bc23b75dd4b8b54ea400
Jarinson Palacios <jyadith@gmail.com>
2023-01-17 16:22:05 -0500

    change access key

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,4 +1,4 @@
-export const ACCESS_KEY = 'OqN38AZmxdoC5cKl-f3_E0Vq268oNsiAcnC8ZvZzfyg'
+export const ACCESS_KEY = '8QWlXw9Sla8t8r7_52pauYLktfzXM5uYyH2xP_cGZXY'
 
 export const API_URL_SEARCH = 'https://api.unsplash.com/search/photos'
 
